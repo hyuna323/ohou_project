35b6de6a923f44030190634eae4799d7d4676cf4
hyunao <89890370+hyunao@users.noreply.github.com>
2022-12-14 04:06:17 +0900

    <오늘의집> 프로젝트 파일 이동

---
+++ js/src/01_prototype_1280_main.js
@@ -0,0 +1,72 @@
+// 01_prototype_1280_main.js
+
+function home(){
+  let url = "http://127.0.0.1:5503/html/01_prototype_1280_main.html";
+  window.location = url;
+}
+
+function login(){
+  let url = "http://localhost:3000/html/02_prototype_1280_loginPage.html";
+  window.location = url;
+}
+
+// 
+var slideIndex = 0;
+
+// html 끝난 후 동작
+window.onload=function(){
+  showSlides(slideIndex);
+
+  const sec = 3000;
+  setInterval(function(){
+    slideIndex++;
+    showSlides(slideIndex);
+
+  }, sec);
+}
+function currentSlide(n) {
+  slideIndex = n;
+  showSlides(slideIndex);
+}
+
+function showSlides(n) {
+
+  const slides = document.getElementsByClassName("viewport");
+  const dots = document.getElementsByClassName("dot");
+  const size = slides.length;
+
+  if ((n+1) > size) {
+    slideIndex = 0; n = 0;
+  }else if (n < 0) {
+    slideIndex = (size-1);
+    n = (size-1);
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[n].style.display = "block";
+  dots[n].className += " active";
+}
+
+// 관련 선택
+//footsubmenu 선택, language part 선택, language_btn선택 ul 선택
+const elfootSubmenu = document.querySelector('.footsubmenu');
+const elBrandsitePart = elfootSubmenu.querySelector('.brandsite_part');
+const BrandsiteBtn = elBrandsitePart.querySelector('.brandsite_btn');
+const Belul = elBrandsitePart.querySelector('ul');
+
+BrandsiteBtn.addEventListener('click', e => {
+  e.preventDefault();
+  let check = Belul.classList.contains('on');
+  (check) ? Belul.classList.remove('on') : Belul.classList.add('on');
+});
+
+elBrandsitePart.addEventListener('mouseleave', e => {
+  e.preventDefault();
+  Belul.classList.remove('on');
+});
---
+++ js/src/02_prototype_1280_loginPage.js
@@ -0,0 +1,9 @@
+function home(){
+  let url = "http://127.0.0.1:5503/html/01_prototype_1280_main.html";
+  window.location = url;
+}
+
+function login(){
+let url = "http://localhost:3000/html/02_prototype_1280_loginPage.html";
+window.location = url;
+}
