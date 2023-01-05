fd469fda73fdbdd37d05b54ad926bec488a47d48
hyunao <89890370+hyunao@users.noreply.github.com>
2023-01-06 05:32:11 +0900

    login page 320px 수정

--- js/src/02_prototype_1280_loginPage.js
+++ js/src/02_prototype_1280_loginPage.js
@@ -7,3 +7,20 @@ function login(){
 let url = "http://localhost:3000/html/02_prototype_1280_loginPage.html";
 window.location = url;
 }
+
+//footsubmenu 선택, language part 선택, language_btn선택 ul 선택
+const elfootSubmenu = document.querySelector('.footsubmenu');
+const elBrandsitePart = elfootSubmenu.querySelector('.brandsite_part');
+const BrandsiteBtn = elBrandsitePart.querySelector('.brandsite_btn');
+const Belul = elBrandsitePart.querySelector('ul');
+
+BrandsiteBtn.addEventListener('click', e => {
+  e.preventDefault();
+  let check = Belul.classList.contains('on');
+  (check) ? Belul.classList.remove('on') : Belul.classList.add('on');
+});
+
+elBrandsitePart.addEventListener('mouseleave', e => {
+  e.preventDefault();
+  Belul.classList.remove('on');
+});
