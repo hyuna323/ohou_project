46520c075149c92f5c1c85e68e7ac622fe0c7727
hyunao <89890370+hyunao@users.noreply.github.com>
2023-01-11 00:43:26 +0900

    페이지 이동 오류 수정

--- js/src/01_prototype_1280_main.js
+++ js/src/01_prototype_1280_main.js
@@ -1,12 +1,12 @@
 // 01_prototype_1280_main.js
 
 function home(){
-  let url = "http://127.0.0.1:5503/html/01_prototype_1280_main.html";
+  let url = "https://hyunao.github.io/ohou_project/html/01_prototype_1280_main.html";
   window.location = url;
 }
 
 function login(){
-  let url = "http://localhost:3000/html/02_prototype_1280_loginPage.html";
+  let url = "https://hyunao.github.io/ohou_project/html/02_prototype_1280_loginPage.html";
   window.location = url;
 }
 
--- js/src/02_prototype_1280_loginPage.js
+++ js/src/02_prototype_1280_loginPage.js
@@ -1,11 +1,11 @@
 function home(){
-  let url = "http://127.0.0.1:5503/html/01_prototype_1280_main.html";
+  let url = "https://hyunao.github.io/ohou_project/html/01_prototype_1280_main.html";
   window.location = url;
 }
 
 function login(){
-let url = "http://localhost:3000/html/02_prototype_1280_loginPage.html";
-window.location = url;
+  let url = "https://hyunao.github.io/ohou_project/html/02_prototype_1280_loginPage.html";
+  window.location = url;
 }
 
 //footsubmenu 선택, language part 선택, language_btn선택 ul 선택
